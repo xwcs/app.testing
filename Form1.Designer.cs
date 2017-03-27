4fa3aebb3ba754a579019b549f82242df7d39cc4
Pavol Pala <palap@ipoint.sk>
2017-03-27 15:12:37 +0200

    work in progress

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,166 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+			this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.panelControl2 = new DevExpress.XtraEditors.PanelControl();
+			this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+			this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+			this.simpleButton4 = new DevExpress.XtraEditors.SimpleButton();
+			this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+			((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+			this.tableLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.panelControl2)).BeginInit();
+			this.panelControl2.SuspendLayout();
+			this.tableLayoutPanel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+			this.panelControl1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// gridControl1
+			// 
+			this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.gridControl1.Location = new System.Drawing.Point(3, 3);
+			this.gridControl1.MainView = this.gridView1;
+			this.gridControl1.Name = "gridControl1";
+			this.gridControl1.Size = new System.Drawing.Size(737, 427);
+			this.gridControl1.TabIndex = 0;
+			this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+			// 
+			// gridView1
+			// 
+			this.gridView1.GridControl = this.gridControl1;
+			this.gridView1.Name = "gridView1";
+			this.gridView1.OptionsBehavior.Editable = false;
+			this.gridView1.OptionsSelection.EnableAppearanceFocusedCell = false;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 93.22459F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.775408F));
+			this.tableLayoutPanel1.Controls.Add(this.panelControl2, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.gridControl1, 0, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(797, 433);
+			this.tableLayoutPanel1.TabIndex = 1;
+			// 
+			// panelControl2
+			// 
+			this.panelControl2.Controls.Add(this.simpleButton3);
+			this.panelControl2.Controls.Add(this.simpleButton2);
+			this.panelControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panelControl2.Location = new System.Drawing.Point(746, 3);
+			this.panelControl2.Name = "panelControl2";
+			this.panelControl2.Size = new System.Drawing.Size(48, 427);
+			this.panelControl2.TabIndex = 3;
+			// 
+			// simpleButton3
+			// 
+			this.simpleButton3.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton3.Image")));
+			this.simpleButton3.Location = new System.Drawing.Point(6, 235);
+			this.simpleButton3.Name = "simpleButton3";
+			this.simpleButton3.Size = new System.Drawing.Size(37, 34);
+			this.simpleButton3.TabIndex = 1;
+			this.simpleButton3.Text = "simpleButton3";
+			this.simpleButton3.Click += new System.EventHandler(this.simpleButton3_Click);
+			// 
+			// simpleButton2
+			// 
+			this.simpleButton2.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton2.Image")));
+			this.simpleButton2.Location = new System.Drawing.Point(6, 195);
+			this.simpleButton2.Name = "simpleButton2";
+			this.simpleButton2.Size = new System.Drawing.Size(37, 34);
+			this.simpleButton2.TabIndex = 0;
+			this.simpleButton2.Text = "simpleButton2";
+			this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.ColumnCount = 1;
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 0);
+			this.tableLayoutPanel2.Controls.Add(this.panelControl1, 0, 1);
+			this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 2;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 92.05021F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.949791F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(803, 478);
+			this.tableLayoutPanel2.TabIndex = 2;
+			// 
+			// panelControl1
+			// 
+			this.panelControl1.Controls.Add(this.simpleButton4);
+			this.panelControl1.Controls.Add(this.simpleButton1);
+			this.panelControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panelControl1.Location = new System.Drawing.Point(3, 442);
+			this.panelControl1.Name = "panelControl1";
+			this.panelControl1.Size = new System.Drawing.Size(797, 33);
+			this.panelControl1.TabIndex = 2;
+			// 
+			// simpleButton4
+			// 
+			this.simpleButton4.Location = new System.Drawing.Point(101, 6);
+			this.simpleButton4.Name = "simpleButton4";
+			this.simpleButton4.Size = new System.Drawing.Size(75, 23);
+			this.simpleButton4.TabIndex = 1;
+			this.simpleButton4.Text = "Save";
+			this.simpleButton4.Click += new System.EventHandler(this.simpleButton4_Click);
+			// 
+			// simpleButton1
+			// 
+			this.simpleButton1.Location = new System.Drawing.Point(10, 6);
+			this.simpleButton1.Name = "simpleButton1";
+			this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+			this.simpleButton1.TabIndex = 0;
+			this.simpleButton1.Text = "Open";
+			this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(803, 478);
+			this.Controls.Add(this.tableLayoutPanel2);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+			this.tableLayoutPanel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.panelControl2)).EndInit();
+			this.panelControl2.ResumeLayout(false);
+			this.tableLayoutPanel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+			this.panelControl1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private DevExpress.XtraGrid.GridControl gridControl1;
+		private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+		private DevExpress.XtraEditors.PanelControl panelControl2;
+		private DevExpress.XtraEditors.SimpleButton simpleButton3;
+		private DevExpress.XtraEditors.SimpleButton simpleButton2;
+		private DevExpress.XtraEditors.PanelControl panelControl1;
+		private DevExpress.XtraEditors.SimpleButton simpleButton1;
+		private DevExpress.XtraEditors.SimpleButton simpleButton4;
 	}
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,81 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Entity;
 
 namespace app.testing
 {
 	public partial class Form1 : Form
 	{
+		private lib.db.doc.niterdoc.NiterDocEntities ctx;
 		public Form1()
 		{
 			InitializeComponent();
+			ctx = new lib.db.doc.niterdoc.NiterDocEntities();
+			ctx.Database.Log = Console.WriteLine;
+		}
+
+		private void simpleButton1_Click(object sender, EventArgs e)
+		{
+			ctx.edizione.Load();
+			refreshGrid();
+		}
+		
+		private void refreshGrid()
+		{
+			string bookmark = "";
+
+			if (gridView1.FocusedRowHandle >= 0) bookmark = gridView1.GetRowCellDisplayText(gridView1.FocusedRowHandle, "id");
+
+			gridControl1.DataSource = ctx.edizione.Where(x => x.nrecord == 30615).ToList().OrderBy(x => x.ordine);
+
+			if (bookmark == "") return;
+
+			int tmp = gridView1.LocateByDisplayText(0, gridView1.Columns["id"], bookmark);
+			if (tmp >= 0)
+			{
+				gridView1.FocusedRowHandle = tmp;
+				gridView1.MakeRowVisible(tmp, true);
+			}
+		}
+
+		private void simpleButton2_Click(object sender, EventArgs e)
+		{
+			int iRowSelect = gridView1.FocusedRowHandle;
+
+			if (iRowSelect >= 0)
+			{
+				lib.db.doc.niterdoc.edizione actualData = gridView1.GetRow(iRowSelect) as lib.db.doc.niterdoc.edizione;
+				lib.db.doc.niterdoc.edizione beforeData = gridView1.GetRow(iRowSelect - 1) as lib.db.doc.niterdoc.edizione;
+
+				if ((beforeData == null) || (actualData.ordine == beforeData.ordine)) return;
+
+				actualData.ordine--;
+				beforeData.ordine++;
+				refreshGrid();
+			}
+		}
+
+		private void simpleButton3_Click(object sender, EventArgs e)
+		{
+			int iRowSelect = gridView1.FocusedRowHandle;
+
+			if (iRowSelect >= 0)
+			{
+				lib.db.doc.niterdoc.edizione actualData = gridView1.GetRow(iRowSelect) as lib.db.doc.niterdoc.edizione;
+				lib.db.doc.niterdoc.edizione nextData = gridView1.GetRow(iRowSelect + 1) as lib.db.doc.niterdoc.edizione;
+
+				if ((nextData == null) || (actualData.ordine == nextData.ordine)) return;
+
+				actualData.ordine++;
+				nextData.ordine--;
+				refreshGrid();
+			}
+		}
+
+		private void simpleButton4_Click(object sender, EventArgs e)
+		{
+			ctx.SaveChanges();
 		}
 	}
 }
