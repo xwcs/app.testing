a338654e687a7527f2cfb8fde1473e1bf9c3b302
Ladislav Sopko <lsopko@3di.it>
2017-06-05 09:47:17 +0200

    Work on GridBindingSource adapter, so we can use Grid and Tree

---
+++ TreeViewForm.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace app.testing
+{
+    partial class TreeViewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeList1 = new DevExpress.XtraTreeList.TreeList();
+            ((System.ComponentModel.ISupportInitialize)(this.treeList1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeList1
+            // 
+            this.treeList1.Location = new System.Drawing.Point(100, 68);
+            this.treeList1.Name = "treeList1";
+            this.treeList1.Size = new System.Drawing.Size(584, 293);
+            this.treeList1.TabIndex = 0;
+            // 
+            // TreeViewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 495);
+            this.Controls.Add(this.treeList1);
+            this.Name = "TreeViewForm";
+            this.Text = "TreeViewForm";
+            ((System.ComponentModel.ISupportInitialize)(this.treeList1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraTreeList.TreeList treeList1;
+    }
+}
