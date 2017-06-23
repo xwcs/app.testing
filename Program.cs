6cf2a831f8656e41da209bf3f2cc70479f58b88e
Ladislav Sopko <lsopko@3di.it>
2017-06-23 09:55:16 +0200

    Fixed Decision states execution

--- Program.cs
+++ Program.cs
@@ -17,11 +17,11 @@ namespace app.testing
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
-            //Application.Run(new lib.db.states.iter.IterSMTestForm());
+            Application.Run(new lib.db.states.iter.IterSMTestForm());
             //Application.Run(new lib.db.states.bt.BtSMTestForm());
 			//Application.Run(new MemoInGridForm());
-           //Application.Run(new TreeViewForm());
-            Application.Run(new InDesign());
+            //Application.Run(new TreeViewForm());
+            //Application.Run(new InDesign());
 			//Application.Run(new Form3_MassiveReplace());
 			//Application.Run(new Form4_EnableDisableControls());
 		}
